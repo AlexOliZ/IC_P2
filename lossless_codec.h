ed3a2b76053016f407f9ff35e628e44b2a02d852
raquel.milheiro2000@gmail.com <raquel.milheiro2000@gmail.com>
2021-12-29 19:55:56 +0000

    mais doxygen

--- lossless_codec.h
+++ lossless_codec.h
@@ -11,74 +11,74 @@ class lossless_codec{
             filename = fname;
         };
         /**
-         * @brief 
+         * @brief convert rgb images to yuv.
          * 
-         * @param image original image matrix
-         * @param y address for the matrix y
-         * @param u address for the matrix u
-         * @param v address for the matrix v
+         * @param image original image matrix.
+         * @param y address for the matrix y.
+         * @param u address for the matrix u.
+         * @param v address for the matrix v.
          */
         void YUV(Mat image, Mat &y, Mat &u, Mat &v);
         /**
-         * @brief 
+         * @brief convert yuv images to rgb.
          * 
-         * @param y address for the matrix y
-         * @param u address for the matrix u
-         * @param v address for the matrix v
-         * @param ImagemRBG final image matrix
+         * @param y address for the matrix y.
+         * @param u address for the matrix u.
+         * @param v address for the matrix v.
+         * @param ImagemRBG final image matrix.
          */
         void RGB (Mat y, Mat u, Mat v, Mat &ImagemRBG);
         /**
-         * @brief 
+         * @brief calculation of the residual to code.
          * 
-         * @param valorPixel pixel value
-         * @param valorPrevisto expected pixel value
-         * @return int -> residual for each value of the predicted pixel
+         * @param valorPixel pixel value.
+         * @param valorPrevisto expected pixel value.
+         * @return int -> residual for each value of the predicted pixel.
          */
         int erroEnc (int valorPixel, int valorPrevisto);
         /**
-         * @brief 
+         * @brief calculation of the decoded pixel value.
          * 
-         * @param erro residual
-         * @param valorPrevisto expected pixel value
-         * @return int -> pixel value for each value of the predicted pixel
+         * @param erro residual.
+         * @param valorPrevisto expected pixel value.
+         * @return int -> pixel value for each value of the predicted pixel.
          */
         int ValorPixelDec (int erro, int valorPrevisto);
         /**
-         * @brief 
+         * @brief predictor the pixel value.
          * 
-         * @param a parameter a to calculate the expected pixel value
-         * @param b parameter b to calculate the expected pixel value
-         * @param c parameter c to calculate the expected pixel value
-         * @return int -> the predicted value of the next pixel
+         * @param a parameter a to calculate the expected pixel value.
+         * @param b parameter b to calculate the expected pixel value.
+         * @param c parameter c to calculate the expected pixel value.
+         * @return int -> the predicted value of the next pixel.
          */
         int preditor (int a, int b, int c);
         /**
-         * @brief 
+         * @brief calculating a,b and c to predict the pixel value.
          * 
-         * @param matriz matrix to calculate the calculated error
-         * @param erMat matrix with the calculated error
-         * @param prev matrix with the calculated predictor
+         * @param matriz matrix to calculate the calculated error.
+         * @param erMat matrix with the calculated error.
+         * @param prev matrix with the calculated predictor.
          */
         void preditor_JPEG_LS (Mat matriz,Mat &erMat, Mat &prev);
         /**
-         * @brief 
+         * @brief encode and write to a file the residual using Golomb and bitstream.
          * 
-         * @param erroY residual of the y parameter to be encoded 
-         * @param erroV residual of the v parameter to be encoded 
-         * @param erroU residual of the u parameter to be encoded 
-         * @param m golomg parameter m
-         * @param namefile name of the file to be write from golomb
+         * @param erroY residual of the y parameter to be encoded.
+         * @param erroV residual of the v parameter to be encoded.
+         * @param erroU residual of the u parameter to be encoded.
+         * @param m golomg parameter m.
+         * @param namefile name of the file to be write from golomb.
          */
         void golombEnc(Mat erroY,Mat erroV,Mat erroU, int m, char* namefile);
         /**
-         * @brief 
+         * @brief dencode and write to a file the residual using Golomb and bitstream.
          * 
-         * @param DescY decoded y matrix
-         * @param DescV decoded v matrix
-         * @param DescU decoded u matrix
-         * @param m golomg parameter m
-         * @param namefile name of the file to be read from golomb
+         * @param DescY decoded y matrix.
+         * @param DescV decoded v matrix.
+         * @param DescU decoded u matrix.
+         * @param m golomg parameter m.
+         * @param namefile name of the file to be read from golomb.
          */
         void golombDesc(Mat &DescY, Mat &DescV, Mat &DescU,int m,char* namefile);
 
